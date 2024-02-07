a746711cb59a96b73736bd8a0757290e26dc6dc1
wacef96 <wacef.stratrait@gmail.com>
2024-02-07 15:51:29 +0100

    added firebase config file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,30 +2,35 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { app, db, getFirestore, collection, addDoc, getDocs } from '../../lib/firebase'; // Adjust the path accordingly
+import { useEffect, useState } from 'react';
+
+
+interface DataType {
+  id: string;
+}
 
 export default function TabOneScreen() {
+  const [data, setData] = useState<DataType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "player"));
+      querySnapshot.forEach((db : any) => {
+        setData((prev) => [...prev, { id: db.id }]);
+        console.log(db.id);
+      });
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      {data.map(item => (
+        <Text key={item.id}>{item.id}</Text> // Customize based on your data structure
+      ))}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
