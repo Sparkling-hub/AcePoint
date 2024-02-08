a8698b823465b7b5be267448dea2538eea083576
wacefEssaies96 <57143245+wacefEssaies96@users.noreply.github.com>
2024-02-08 08:44:37 +0000

    Ac 43 enable firebase in the project (#2)
    * added firebase config file
    
    * redo index.tsx

---
+++ lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCGuHH-y3rBoWAO62Fov0nBWARVaRpG8Nw",
+    authDomain: "acepoint-9cd79.firebaseapp.com",
+    projectId: "acepoint-9cd79",
+    storageBucket: "acepoint-9cd79.appspot.com",
+    messagingSenderId: "739771999940",
+    appId: "1:739771999940:web:aa001b5d5c4dcd96fd8b91",
+    measurementId: "G-2MCQY7XRP6"
+}
+
+// Initialize Firebase Admin with service account credentials
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app);
+
+export { app, db, getFirestore, collection, addDoc, getDocs };
