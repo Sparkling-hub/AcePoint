ec8ad99f1244ce55afe3e1ec44262b2e0c9bfc42
dhiabj <dhia.bejaoui.db@gmail.com>
2024-02-22 17:27:38 +0100

    fix issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,16 @@
-import Login from '@/screens/Login'
-import { SafeAreaView, StyleSheet } from 'react-native'
+import Login from '@/screens/Login';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 export default function login() {
-
- 
-    return (
-        <SafeAreaView style={styles.container}>
-             <Login /> 
-            {/* {!isInputFocused && <View style={{marginTop:30}}><HeaderArrow data={"CLUB"}  gap={"$12"} /></View>} */}
-            
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <Login />
+    </SafeAreaView>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-
-    }
-  })
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
