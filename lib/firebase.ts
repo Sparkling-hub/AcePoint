4033c8c414a3584b9685f007e9d937ec1cd05b94
wacef96 <wacef.stratrait@gmail.com>
2024-02-26 11:43:09 +0100

    fixed firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,16 +3,17 @@ import { getFirestore, collection, addDoc, getDocs, doc, setDoc, updateDoc } fro
 import { getReactNativePersistence, signOut, initializeAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { ref, getDownloadURL, uploadBytesResumable, getStorage } from 'firebase/storage'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASSUREMENT_ID } from '@env';
 
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASSUREMENT_ID
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASSUREMENT_ID
 }
 
 // Initialize Firebase Admin with service account credentials
@@ -28,6 +29,6 @@ export {
     ref, getDownloadURL, uploadBytesResumable, getFirestore,
     collection, addDoc, doc, setDoc, getDocs, createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-    signOut,updateDoc
-    
+    signOut, updateDoc
+
 };
