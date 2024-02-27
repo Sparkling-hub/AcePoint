505ca27592ce2497100724a27c46b08abd8230c5
HazemGhannem <hazem.ghannem@esprit.tn>
2024-02-27 13:41:04 +0100

    AC-74

---
+++ api/player-api.ts
@@ -0,0 +1,27 @@
+import {db } from '@/lib/firebase'
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+
+const findByName = async ({name}: { name: string }) => {
+    try {
+        if(name.length !==0){
+            
+        const data = query(collection(db, 'club'), where('name', '==', name));
+        const clubs = await getDocs(data);
+        if (clubs.empty) {
+            // return there is no name if name dos not exist in db
+            return "there is no name"
+        }
+        // return the filtered data
+        return clubs
+        
+        }
+        // return the all data if there is no name
+        const data = collection(db, 'club');
+        const clubs = await getDocs(data);
+       return clubs
+    } catch (error: any) {
+        console.error('get failed:', error.message);
+    }
+};
+export { findByName}
