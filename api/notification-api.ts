fe2c5f16add8ef7af6f00cee212b2d94d410fb2e
wacefEssaies96 <wacef.stratrait@gmail.com>
2024-03-06 14:55:44 +0100

    documented the api

--- api/notification-api.ts
+++ api/notification-api.ts
@@ -4,12 +4,18 @@ import { retrieveData } from "./localStorage";
 
 const findOrCreateNotification = async (type: string, checked: boolean) => {
     try {
+        // Retrieve user's email and user type from local storage
         const email = await retrieveData('email')
         const userType = await retrieveData('userType')
+
+        // Determine the collection based on the user type
         const userCollection = userType === 'Player' ? collection(db, 'player') : collection(db, 'coach')
+
+        // Query Firestore to find the user document with the matching email
         const notificationQuery = query(userCollection, where('email', '==', email))
         const result = await getDocs(notificationQuery)
 
+        // If the user document exists, update it with the specified type and checked values
         if (!result.empty) {
             result.docs.forEach(async (element) => {
                 await updateDoc(element.ref, {
@@ -19,6 +25,7 @@ const findOrCreateNotification = async (type: string, checked: boolean) => {
             });
         }
     } catch (error) {
+        // Handle errors by logging them to the console
         console.error('Error finding or creating notification:', error);
     }
 };
