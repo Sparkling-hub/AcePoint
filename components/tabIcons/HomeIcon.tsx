cc9315fd244a50b8f06fb0286fa287184cec7b85
dhiabj <dhia.bejaoui.db@gmail.com>
2024-03-12 13:16:30 +0100

    fix issues

--- components/tabIcons/HomeIcon.tsx
+++ components/tabIcons/HomeIcon.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Home from '@/components/svg/Home';
 import Colors from '@/constants/Colors';
-import { RootState } from '@/store/store';
 
 const HomeIcon = ({ focused }: { focused: boolean }) => {
   return <Home fill={focused ? Colors.secondary : Colors.primary} />;
