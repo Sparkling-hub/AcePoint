a96c9c7de468d6f0f128e8a7abd503603f0f4657
dhiabj <dhia.bejaoui.db@gmail.com>
2024-03-15 18:17:24 +0100

    added nearbyclubs map feature

--- components/NearbyClubsMap.tsx
+++ components/NearbyClubsMap.tsx
@@ -1,15 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { View } from 'tamagui';
+import MapView, {
+  Callout,
+  Marker,
+  PROVIDER_GOOGLE,
+  Region,
+} from 'react-native-maps';
+import { Spinner, Text, View } from 'tamagui';
 
 import CustomLocationButton from './CustomLocationButton';
 import * as Location from 'expo-location';
 import fireToast from './toast/Toast';
+import { Club } from '@/model/club';
+import { distanceCalculation } from '@/api/player-api';
+import Colors from '@/constants/Colors';
 
 const NearbyClubsMap = () => {
+  const [currentLocation, setCurrentLocation] = useState<Region>();
+  const [nearbyClubs, setNearbyClubs] = useState<Club[]>([]);
+  const [selectedMarkerId, setSelectedMarkerId] = useState<string | null>(null);
   const mapRef = useRef<MapView>(null);
-  const handleLocationPress = async () => {
+  const [loading, setLoading] = useState(true);
+
+  const fetchNearbyClubs = async () => {
     try {
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -25,32 +38,78 @@ const NearbyClubsMap = () => {
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
       };
-      // Center the map to the user's current location
-      mapRef?.current?.animateToRegion(region);
+      setCurrentLocation(region);
+      const clubs: any = await distanceCalculation(latitude, longitude, 50);
+      setNearbyClubs(clubs);
     } catch (error) {
       fireToast({
         message: 'Please enable location services',
         type: 'error',
       });
+    } finally {
+      setLoading(false);
     }
+  };
+
+  useEffect(() => {
+    fetchNearbyClubs();
+  }, []);
+
+  const handleLocationPress = () => {
+    if (currentLocation) {
+      mapRef.current?.animateToRegion(currentLocation, 1000);
+    }
+  };
+
+  const handleMarkerPress = (markerId: string) => {
+    setSelectedMarkerId(markerId === selectedMarkerId ? null : markerId);
   };
 
   return (
     <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        showsUserLocation={true}
-        showsMyLocationButton={false}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-      <CustomLocationButton onPress={handleLocationPress} />
+      {loading ? (
+        <Spinner size="large" color={Colors.secondary} />
+      ) : (
+        <>
+          <MapView
+            ref={mapRef}
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            showsUserLocation={true}
+            showsMyLocationButton={false}
+            initialRegion={currentLocation}>
+            {nearbyClubs.map((club) => (
+              <Marker
+                key={club.id}
+                coordinate={{
+                  latitude: club.latitude || 0,
+                  longitude: club.longitude || 0,
+                }}
+                onPress={() => handleMarkerPress(club.id || '')}
+                image={
+                  club.id === selectedMarkerId
+                    ? require('../assets/images/location_on.png')
+                    : require('../assets/images/location.png')
+                }>
+                <Callout tooltip>
+                  <View>
+                    <View
+                      height={32}
+                      paddingHorizontal={13}
+                      alignItems="center"
+                      justifyContent="center"
+                      borderRadius={20}
+                      backgroundColor={'rgba(58, 77, 108, 0.9)'}>
+                      <Text color={'white'}>{club.name}</Text>
+                    </View>
+                  </View>
+                </Callout>
+              </Marker>
+            ))}
+          </MapView>
+          <CustomLocationButton onPress={handleLocationPress} />
+        </>
+      )}
     </View>
   );
 };
